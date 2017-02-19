bb80d33df2d64014615993fbed7e5760e9cd20c7
Rushil <rushil0195@gmail.com>
2017-02-20 01:28:58 +0530

    svm-weights-factor

---
+++ k-means/R/svm-factor-weights.R
@@ -0,0 +1,6 @@
+library(rpart)
+w <- t(svm.model$coefs) %*% svm.model$SV  
+w <- apply(w, 2, function(v){sqrt(sum(v^2))})
+w <- sort(w, decreasing = T)
+print(w)
+write.csv(w, file = "factor-weights.csv")
