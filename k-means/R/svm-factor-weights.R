50a99ef7e19f04a4cbbcea49230613d42492ceac
Rushil <rushil0195@gmail.com>
2017-03-14 01:27:50 +0530

    Folder hierarchy -2

--- k-means/R/svm-factor-weights.R
+++
@@ -1,6 +0,0 @@
-library(rpart)
-w <- t(svm.model$coefs) %*% svm.model$SV  
-w <- apply(w, 2, function(v){sqrt(sum(v^2))})
-w <- sort(w, decreasing = T)
-print(w)
-write.csv(w, file = "factor-weights.csv")
